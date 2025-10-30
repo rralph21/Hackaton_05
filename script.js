69a55f33411c882ee06a6823167a703a9c59e37e
Ralph.Vitug <rralph@rrc.ca>
2025-10-30 10:18:47 -0500

    add: script:Async funtion html:drop down opt css:background image

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showPromiseExample() {
+ function showPromiseExample() {
   console.log("Start of code");
 
 let myPromise = new Promise((resolve, reject) => {
@@ -15,3 +15,53 @@ myPromise.then((message) => {
 }
 
 showPromiseExample();
+
+async function fetchStockList() {
+  try {
+let response = await fetch("https://catfact.ninja/breeds");
+
+  if (!response.ok) {
+  throw new Error("Something went wrong while getting breeds.");
+}
+let data = await response.json();
+  console.log("Cat breeds data:", data);
+  } catch (error) {
+  console.log("Error:", error);
+  }
+}
+
+fetchStockList();
+
+function getSelectedStock() {
+  let dropdown = document.getElementById("coinCount");
+  return dropdown.value; 
+}
+
+async function displayStockList() {
+
+  let count = getSelectedStockCount();
+  let stocksContainer = document.getElementById("stockContainer");
+
+  stocksContainer.innerHTML = "Loading stocks...";
+
+  try {
+    let response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json=${count}`);
+
+    if (!response.ok) {
+      throw new Error("Could not fetch stock.");
+    }
+
+    let data = await response.json();
+
+    stocksContainer.innerHTML = "";
+
+    data.data.forEach((fact) => {
+      let p = document.createElement("p");
+      p.textContent = fact.fact;
+      stocksContainer.appendChild(p);
+    });
+  } catch (error) {
+    stocksContainer.innerHTML = "Sorry, something went wrong.";
+    console.log("Error:", error);
+  }
+}
